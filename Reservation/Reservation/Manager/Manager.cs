0b477e9545724d3d798cd9fc74ff844b08eb74c1
arpi <arpiarustamyan21@gmail.com>
2019-10-01 14:10:29 +0400

    Add Find and RemoveById

---
+++ Reservation/Reservation/Manager/Manager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservation.Manager
+{
+    public class Manager
+    {
+        public Object Find(int id, string table)
+        {
+            using (ReservContext db = new ReservContext())
+            {
+                var t = db.Set(System.Type.GetType(table)).Find(id);
+                return t;          
+             }
+        }
+
+        public Object RemoveById(int id, string table)
+        {
+            using (ReservContext db = new ReservContext())
+            {
+                var t = db.Set(System.Type.GetType(table)).Remove(id);
+                return t;
+            }
+        }
+    }
+}
