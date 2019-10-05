fd96bcada00a0aae7a0f36dcd9e94f372698a90e
arpi <arpiarustamyan21@gmail.com>
2019-10-05 14:38:47 +0400

    Add Technic in EntityModel

---
+++ Reservation/EntityModel/EntityModels/Technic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityModel.EntityModels
+{
+    public class Technic
+    {
+        public string _Name { set; get; }
+        public int _Count { set; get; }
+    }
+}
