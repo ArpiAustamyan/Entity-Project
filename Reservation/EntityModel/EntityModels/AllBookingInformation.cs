58ff814c5c3f3b9a0b6dbc72d3a587788f631a04
arpi <arpiarustamyan21@gmail.com>
2019-10-05 14:03:39 +0400

    Add EntityModel.cs

---
+++ Reservation/EntityModel/EntityModels/AllBookingInformation.cs
@@ -0,0 +1,20 @@
+﻿using Reservation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservation.EntityModels
+{
+    public class AllBookingInformation
+    {
+        public string _FullName { set; get; }
+        public int _RoomNumber { set; get; }
+        public List<RoomFurniture> _FurnitureList { set; get; }
+        public List<BookingTechnic> _TechnicList { set; get; }
+        public DateTime _StartTime { set; get; }
+        public DateTime _EndTime { set; get; }
+        public int _Cost { get; set; }
+    }
+}
---
+++ Reservation/EntityModel/EntityModels/BookingModel.cs
@@ -0,0 +1,19 @@
+﻿using Reservation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservation.EntityModels
+{
+    public class BookingModel
+    {
+        public int _RoomNumber { set; get; }
+        public int _UserId { set; get; }
+        public List<int> _Users { set; get; }
+        public List<Furniture> _Furn { set; get; }
+        public DateTime _StratTime { set; get; }
+        public DateTime _EndTime { set; get; }
+    }
+}
