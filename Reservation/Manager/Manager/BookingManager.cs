d124576e5b5c48a5e6e7fef1e891225274ef8f0d
arpi <arpiarustamyan21@gmail.com>
2019-10-05 14:03:03 +0400

    Add Manager.cs

---
+++ Reservation/Manager/Manager/BookingManager.cs
@@ -0,0 +1,110 @@
+﻿using Reservation.EntityModels;
+using Reservation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservation
+{
+    public class BookingManager : Manager.Manager, IDisposable
+    {
+
+        public void Add(BookingModel model)
+        {
+            Booking booking = new Booking();
+            booking.StartTime = model._StratTime;
+            booking.EndTime = model._EndTime;
+            booking.RoomId = model._RoomNumber;
+            booking.UserId = model._UserId;
+
+            var booking_user = model._Users.Select(i => new BookingUser
+            {
+                Booking = booking,
+                UserId = i
+            });
+            var some = db.BookingUsers.AddRange(booking_user);
+        }
+
+        public Booking Find(int id)
+        {
+            Booking b = (Booking)Find(id, "Bookings");
+            return b;
+        }
+
+        public Booking RemoveById(int id)
+        {
+            Booking b = (Booking)RemoveById(id, "Bookings");
+            return b;
+        }
+
+        public Booking[] SearchBookings(string userNameStart)
+        {
+            var booking = (from b in db.Bookings
+                           join u in db.Users on b.UserId equals u.Id
+                           where u.Name.StartsWith(userNameStart)
+                           select b)
+                           .ToArray();
+            return booking;
+        }
+
+        public Booking[] SearchBookings(string userNameStart, int cost)
+        {
+            var booking = (from b in db.Bookings
+                           join u in db.Users on b.UserId equals u.Id
+                           where u.Name.StartsWith(userNameStart)
+                           join r in db.Rooms on b.RoomId equals r.Id
+                           where r.Price == cost
+                           select b)
+                           .ToArray();
+            return booking;
+        }
+
+        public AllBookingInformation BookingInformation(int id)
+        {
+            var booking = from b in db.Bookings
+                          join bt in db.BookingTechnics on b.Id equals bt.BookingId
+                          group bt by b into gr
+                          join rf in db.RoomFurnitures on gr.Key.Id equals rf.RoomId
+                          join r in db.Rooms on rf.FurnitureId equals r.Id
+                          group rf by new { gr, r } into gr1
+                          where gr1.Key.gr.Key.Id == id
+                          select new AllBookingInformation
+                          {
+                              _FullName = gr1.Key.gr.Key.User.Name + " /t" + gr1.Key.gr.Key.User.LastName,
+                              _RoomNumber = gr1.Key.r.Id,
+                              _FurnitureList=gr1.ToList(),
+                              _TechnicList = gr1.Key.gr.ToList(),
+                              _StartTime=gr1.Key.gr.Key.StartTime,
+                              _EndTime = gr1.Key.gr.Key.EndTime,
+                              _Cost = gr1.Key.r.Price +
+                              gr1.Sum(i => i.Count * i.Furniture.HourlyCost) * (gr1.Key.gr.Key.EndTime.Hour - gr1.Key.gr.Key.StartTime.Hour) +
+                              gr1.Key.gr.Sum(i => i.Count * i.Furniture.HourlyCost) * (gr1.Key.gr.Key.EndTime.Hour - gr1.Key.gr.Key.StartTime.Hour)
+                          };
+            return booking.FirstOrDefault();
+        }
+
+        private bool _disposed;
+
+        ReservContext db = new ReservContext();
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+
+                }
+            }
+            _disposed = true;
+        }
+    }
+}
---
+++ Reservation/Manager/Manager/UserManager.cs
@@ -0,0 +1,126 @@
+﻿using Reservation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Reservation.ReservationInfo;
+
+namespace Reservation
+{
+    public class UserManager : Manager.Manager
+    {
+        public User Find(int id)
+        {
+            using (ReservContext db = new ReservContext())
+            {
+                User u = (User)Find(id,"Users");
+                return u;
+            }
+        }
+
+        public User RemoveById(int id)
+        {
+            User u = (User)RemoveById(id, "Users");
+            return u;
+        }
+
+        public void Add(User user)
+        {
+            if (user.Name.Length >= 3 && user.LastName.Length >= 3)
+            {
+                using (ReservContext db = new ReservContext())
+                {
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public void ChangeName(string fName, string lName, int id)
+        {
+            if (fName.Length >= 3 && lName.Length >= 3)
+            {
+                using (ReservContext db = new ReservContext())
+                {
+                    var user = db.Users.FirstOrDefault<User>(u => u.Id == id);
+                    if (user != null)
+                    {
+                        user.Name = fName;
+                        user.LastName = lName;
+                    }
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (ReservContext db = new ReservContext())
+            {
+                var user = db.Users.FirstOrDefault<User>(u => u.Id == id);
+                db.Users.Remove(user);
+                db.SaveChanges();
+            }
+        }
+
+        public IQueryable GetUsersWithChar(char firstInitial)
+        {
+            using (ReservContext db = new ReservContext())
+            {
+                var user = db.Users.Where<User>
+                    (u => u.Name.Contains(firstInitial));
+                db.SaveChanges();
+                return user;
+            }
+
+        }
+
+        public PersonInfo[] GetUserReservations()
+        {
+            using (ReservContext db = new ReservContext())
+            {
+                var personinfo = from user in db.Users
+                                 join booking in db.BookingUsers on user.Id equals booking.UserId
+                                 group booking by user into gr
+                                 select new PersonInfo
+                                 {
+                                     _Name = gr.Key.Name,
+                                     _Balance = gr.Key.Balance,
+                                     _ReservInfo = gr.Select(i => new ReservationInfo
+                                     {
+                                         _RoomId = i.Booking.RoomId,
+                                         _ReservStart = i.Booking.StartTime
+                                     })
+                                 };
+
+
+                return personinfo.ToArray<PersonInfo>();
+            }
+        }
+
+
+
+        //private bool _disposed;
+
+        //ReservContext db = new ReservContext();
+        //public void Dispose()
+        //{
+        //    Dispose(true);
+        //    GC.SuppressFinalize(this);
+        //}
+
+        //protected virtual void Dispose(bool disposing)
+        //{
+        //    if (!_disposed)
+        //    {
+        //        if (disposing)
+        //        {
+        //            db.Dispose();
+
+        //        }
+        //    }
+        //    _disposed = true;
+        //}
+    }
+}
