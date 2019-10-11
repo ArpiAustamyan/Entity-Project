0213838e83dbe49c80149f488b286a719a17288a
arpi <arpiarustamyan21@gmail.com>
2019-10-11 12:58:12 +0400

    Add RoomModel

---
+++ Reservation/EntityModel/EntityModels/RoomModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityModel.EntityModels
+{
+    public class RoomModel
+    {
+        public int _Id { set; get; }
+        public int _Price { set; get; }
+        public IEnumerable<FurnitureModel> _FurList { set; get; }
+        public int? _InnerId { set; get; }
+    }
+}
