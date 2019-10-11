2f738ec5278c11e5c64224b92dda04a9e02644ce
arpi <arpiarustamyan21@gmail.com>
2019-10-11 12:37:03 +0400

    Add FurnitureModel

---
+++ Reservation/EntityModel/EntityModels/FurnitureModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityModel.EntityModels
+{
+    public class FurnitureModel
+    {
+        public int _Id { set; get; }
+        public int _Count { set; get; }
+    }
+}
