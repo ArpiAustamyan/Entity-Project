f1c3f567f9541ce8b382882d60d230af61a1181f
arpi <arpiarustamyan21@gmail.com>
2019-10-13 21:54:23 +0400

    Add UserModel

---
+++ Reservation/EntityModel/EntityModels/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityModel.EntityModels
+{
+    public class UserModel
+    {
+        public int _Id { set; get; }
+        public string _FirstName { set; get; }
+        public string _LastName { set; get; }
+        public List<int> _Friends { set; get; }
+    }
+}
