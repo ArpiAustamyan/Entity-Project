fd023c989ac913d05fe33747e07c9286333b17bf
arpi <arpiarustamyan21@gmail.com>
2019-10-13 22:14:53 +0400

    Add GetRooms, Edit and Add Functions

--- Reservation/Manager/Manager/RoomManager.cs
+++ Reservation/Manager/Manager/RoomManager.cs
@@ -1,6 +1,8 @@
-﻿using Reservation.Models;
+﻿using EntityModel.EntityModels;
+using Reservation.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +15,7 @@ namespace Reservation
         {
             using (ReservContext db = new ReservContext())
             {
-                Room r =(Room)Find( id,"Rooms");
+                Room r = (Room)Find(id, "Rooms");
                 return r;
             }
         }
@@ -40,5 +42,63 @@ namespace Reservation
             }
 
         }
+
+        public void Add(RoomModel model)
+        {
+            using (ReservContext db = new ReservContext())
+            {
+                Room room = new Room
+                {
+                    Price = model._Price,
+                    RoomId = model._InnerId
+                };
+
+                var room_furn = model._FurList.Select(i => new RoomFurniture
+                {
+                    Room = room,
+                    FurnitureId = i._Id,
+                    Count = i._Count
+                });
+
+                db.RoomFurnitures.AddRange(room_furn);
+                db.Rooms.Add(room);
+            }
+        }
+
+        public void Edit(RoomModel model)
+        {
+            using (ReservContext db = new ReservContext())
+            {
+                var room_edit = db.Rooms.Where(i => i.Id == model._Id).FirstOrDefault();
+                room_edit.Price = model._Price;
+                room_edit.RoomId = model._InnerId;
+                db.SaveChanges();
+                //db.Entry(room_edit).State = EntityState.Modified;
+            }
+        }
+
+        public RoomModel[] GetRooms()
+        {
+            using (ReservContext db = new ReservContext())
+            {
+                var rm = (from r in db.Rooms
+                          join rf in db.RoomFurnitures
+                          on r.Id equals rf.FurnitureId
+                          group rf by r into gr
+                          select new RoomModel
+                          {
+                              _Id = gr.Key.Id,
+                              _Price = gr.Key.Price,
+                              _FurList = gr.Select(i => new FurnitureModel
+                              {
+                                  _Id = i.FurnitureId,
+                                  _Count = i.Count
+                              })
+                          }).ToArray();
+
+                return rm;
+            }
+        }
+
     }
 }
