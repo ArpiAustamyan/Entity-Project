d4f25234c5a6bb7295a551744930bd4662ba8b24
arpi <arpiarustamyan21@gmail.com>
2019-10-13 22:16:52 +0400

    Add GetFurnitures, Edit and Add Functions

--- Reservation/Manager/Manager/FurnitureManager.cs
+++ Reservation/Manager/Manager/FurnitureManager.cs
@@ -1,4 +1,5 @@
-﻿using Reservation.Models;
+﻿using EntityModel.EntityModels;
+using Reservation.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +8,41 @@ using System.Threading.Tasks;
 
 namespace Reservation.Manager
 {
-     public class FurnitureManager: Manager, IDisposable
-     {
+    public class FurnitureManager : Manager, IDisposable
+    {
+        public void Add(FurnitureModel model)
+        {
+            Furniture furn = new Furniture
+            {
+                HourlyCost = model._Price,
+                Name = model._Name
+            };
+            db.Furnitures.Add(furn);
+        }
+
+        public void Edit(FurnitureModel model)
+        {
+            var furn_edit = db.Furnitures.Where(i => i.Id == model._Id).FirstOrDefault();
+            furn_edit.HourlyCost = model._Price;
+            db.SaveChanges();
+        }
+
+        public FurnitureModel[] GetFurnitures()
+        {
+            var rm = (from f in db.Furnitures
+                      select new FurnitureModel
+                      {
+                          _Id = f.Id,
+                          _Name = f.Name,
+                          _Price = f.HourlyCost
+                      }).ToArray();
+
+            return rm;
+        }
+
         public Furniture Find(int id)
         {
-            Furniture f = (Furniture)Find( id, "Furnitures");
+            Furniture f = (Furniture)Find(id, "Furnitures");
             return f;
         }
 
